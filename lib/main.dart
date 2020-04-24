c9c9b07d24fc1ceac15a2775db51b13d2ecca918
MuhammadEssam96 <muhammad.essam96@gmail.com>
2020-04-24 17:06:08 +0200

    Check if location is stored in SharedPreferences and LocationPermission

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:muslim_prayer_times/ui/Screens/home_screen.dart';
-import 'package:muslim_prayer_times/ui/Values/themes.dart';
-import 'package:muslim_prayer_times/ui/screens/set_location_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:muslim_prayer_times/services/shared_preferences_service.dart';
+import 'package:muslim_prayer_times/ui/values/themes.dart';
+import 'package:muslim_prayer_times/ui/screens/home_screen.dart';
+import 'package:muslim_prayer_times/ui/screens/set_location_automatically_screen.dart';
+import 'package:muslim_prayer_times/ui/screens/set_location_main_screen.dart';
+import 'package:muslim_prayer_times/ui/screens/set_locaton_manually_screen.dart';
+import 'package:provider/provider.dart';
+
+import 'models/location_permission_model.dart';
 
 void main() => runApp(MuslimPrayerTimesApp());
 
 class MuslimPrayerTimesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
-      title: "Muslim Prayer Times",
-      debugShowCheckedModeBanner: false,
-      theme: AppThemes.appTheme,
-      home: FutureBuilder(
-        future: checkIfLocationSaved(),
-        builder: (BuildContext buildContext, AsyncSnapshot<bool> snapshot) {
-          return snapshot.data == true ? HomeScreen() : SetLocationScreen();
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => LocationPermission())
+      ],
+      child: MaterialApp(
+        title: "Muslim Prayer Times",
+        debugShowCheckedModeBanner: false,
+        theme: AppThemes.appTheme,
+        home: FutureBuilder(
+          future: SharedPreferencesService.checkIfLocationSaved(),
+          builder: (BuildContext buildContext, AsyncSnapshot<bool> snapshot) {
+            return snapshot.data == true ? HomeScreen() : SetLocationMainScreen();
+          }
+        ),
+        routes: {
+          '/home' : (context) => HomeScreen(),
+          '/setLocationMain' : (context) => SetLocationMainScreen(),
+          '/setLocationAutomatically' : (context) => SetLocationAutomaticallyScreen(),
+          '/setLocationManually' : (context) => SetLocationManuallyScreen(),
         }
       ),
-      routes: {
-        '/home' : (context) => HomeScreen(),
-        '/editLocation' : (context) => SetLocationScreen(),
-      }
     );
   }
-
-  Future<bool> checkIfLocationSaved() async {
-  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-  return sharedPreferences.getBool('locationIsSaved');
 }
-}
-
-
